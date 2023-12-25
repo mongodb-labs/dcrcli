885f86b6a3a8943dc639d3965bb154488e03afde
nishant <nishant@mongodb.com>
2023-12-25 15:50:58 +0530

    If mongosh available then launch it with mongoWellnessChecker else if mongo shell is available launch it with getMongoData   else exit with a message

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dcrcli
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"dcrcli/mongo"
+	"dcrcli/mongosh"
+)
+
+func main() {
+	// prefer mongosh first if available else fallback to mongo shell and getMongoData
+	if !mongosh.Detect() {
+		if !mongo.Detect() {
+			fmt.Println(
+				"O Oh: Could not find the mongosh or legacy mongo shell. Install that first.",
+			)
+		}
+		mongo.Run("user", "pass", "mongodb://localhost:27017")
+	}
+	mongosh.Run("user", "pass", "mongodb://localhost:27017")
+}
